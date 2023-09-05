e3a164b29dd786e6296a9335969c0003eb7da5e1
Thanawat <devgameby123@gmail.com>
2023-09-05 12:49:47 +0700

    firstcommit

---
+++ components/navbar.js
@@ -0,0 +1,15 @@
+export default function Navbar(){
+    return (
+        <>
+            <div>
+                Josh Shop
+            </div>
+            <ul>
+                <li>Link1</li>
+                <li>Link2</li>
+                <li>Link3</li>
+                <li>Link4</li>
+            </ul>
+        </>
+    )
+}
